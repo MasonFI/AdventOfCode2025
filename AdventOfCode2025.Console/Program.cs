be3e05ce08c6c63d295a11d0e52d12aafaef20de
joonasmanninen <joonas.manninen@kehatieto.fi>
2025-12-01 15:11:04 +0200

    Konsolisovellus jolla voidaan ajaa halutun päivämäärän tehtävä

---
+++ AdventOfCode2025.Console/Program.cs
@@ -0,0 +1,74 @@
+﻿using AdventOfCode2025.Application.Day1;
+
+namespace ConsoleApp
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Anna päivän numero (1–12): ");
+            var input = Console.ReadLine();
+
+            // Yritetään muuntaa syöte numeroksi
+            if (!int.TryParse(input, out int paiva))
+            {
+                Console.WriteLine("Syötteen tulee olla numero.");
+                return;
+            }
+
+            // Tarkistetaan että luku on välillä 1–12
+            if (paiva < 1 || paiva > 12)
+            {
+                Console.WriteLine("Päivän numeron tulee olla välillä 1–12.");
+                return;
+            }
+
+            // Suoritetaan päivän koodi
+            RunDay(paiva);
+        }
+
+        static void RunDay(int paiva)
+        {
+            switch (paiva)
+            {
+                case 1:
+                    Console.WriteLine("Suoritetaan päivän 1 koodi...");
+
+                    var instructionsParser = new InstructionsParserService();
+                    
+                    var instructions = instructionsParser.ParseFromFile("day1.txt");
+                    var wheelService = new WheelService();
+
+                    foreach (var instruction in instructions)
+                    {
+                        wheelService.RotateWheel(instruction.Direction, instruction.NumberOfRotations);    
+                    }
+                    
+                    Console.WriteLine(wheelService.TimesWheelRotatedToZero);
+                    
+                    break;
+
+                case 2:
+                    Console.WriteLine("Suoritetaan päivän 2 koodi...");
+                    // TODO
+                    break;
+
+                case 3:
+                    Console.WriteLine("Suoritetaan päivän 3 koodi...");
+                    // TODO
+                    break;
+
+                // ...
+
+                case 12:
+                    Console.WriteLine("Suoritetaan päivän 12 koodi...");
+                    // TODO
+                    break;
+
+                default:
+                    Console.WriteLine("Tuntematon päivä.");
+                    break;
+            }
+        }
+    }
+}
