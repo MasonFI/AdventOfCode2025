51af8167b2bdae710714e63369557451a11c8420
joonasmanninen <joonas.manninen@kehatieto.fi>
2025-12-01 15:10:48 +0200

    InstructionParserService Day1.txt syötteen parsimista varten

---
+++ AdventOfCode2025.Application/Day1/InstructionsParserService.cs
@@ -0,0 +1,42 @@
+namespace AdventOfCode2025.Application.Day1;
+
+public class RotationInstruction
+{
+    public IWheelService.RotationDirection Direction { get; set; }
+
+    public int NumberOfRotations { get; set; }
+}
+
+public class InstructionsParserService
+{
+    public IEnumerable<RotationInstruction> ParseFromFile(string filepath)
+    {
+        var instructions = new List<RotationInstruction>();
+        foreach (var line in File.ReadLines(filepath))
+        {
+            if (string.IsNullOrWhiteSpace(line))
+                continue;
+
+            var directionChar = line[0];
+            var direction = directionChar switch
+            {
+                'L' => IWheelService.RotationDirection.Left,
+                'R' => IWheelService.RotationDirection.Right,
+                _ => throw new Exception($"Virheellinen rivi, suuntaa ei voida lukea: {line}. ")
+            };
+            
+            var numberPart = line[1..];
+
+            if (!int.TryParse(numberPart, out int steps))
+            {
+                Console.WriteLine($"Virheellinen rivi, steppien määrää ei voida lukea: {line}");
+
+                continue;
+            }
+            
+            instructions.Add(new RotationInstruction { Direction = direction, NumberOfRotations = steps });
+        }
+
+        return instructions;
+    }
+}
